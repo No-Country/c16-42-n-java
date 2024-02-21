8895ed5447391a8c6041857fb731a6028b7e187b
panchoalenda <pancho_alenda@yahoo.com.ar>
2024-02-21 11:09:52 -0300

    Creando Landing Page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Banner from './components/Banner'
+import { Navbar } from './components/NavBar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-        <h1>Testing</h1>
+       <Banner/>
+       <Navbar/>
     </>
   )
 }
---
+++ Frontend/src/components/Banner.jsx
@@ -0,0 +1,23 @@
+
+import '../components/Banner.css'
+function Banner() {
+
+  return (
+    <>
+      <section className="hero">
+        <div className="mascara">
+          <div className="container">
+            <div className="row">
+      <img className='rounded float-start' src="./img/logo.png" alt="" />
+              <div className="col text-end">
+               
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Banner;
